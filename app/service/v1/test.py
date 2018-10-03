3692aceb1add14c25424bf86a6f4082cbca6446e
haohsfb <haohsfb@126.com>
2018-10-03 14:05:01 +0800

    update

--- app/service/v1/test.py
+++ app/service/v1/test.py
@@ -14,9 +14,27 @@ from app.comm.get_form_html import get_text
 from app.comm.log import Log
 
 
-@test.route('/v1/checkOrderExist', methods=['GET', 'POST'])
+@test.route('/', methods=['GET', 'POST'])
 def check_order_exist():
     return jsonify({"data": "ok"})
+
+
+@test.route('/get', methods=['GET'])
+def get_ddd():
+    with open('story.json', mode='r', encoding='utf-8')as f:
+        data = json.load(f)
+        for item in data.get('data'):
+            result = get_text(item.get('name'), item.get('domain_name'), item.get('url'), item.get('latest_chapter'))
+            if result:
+                item['latest_chapter'] = result
+            Log.info('%s 检查完毕。。。' % item.get('name'))
+
+    with open('story.json', mode='w', encoding='utf-8')as f:
+        f.write(json.dumps(data))
+        Log.info('json更新完毕。。。')
+    Log.info('全部检查完毕')
+    Log.info("---" * 20)
+    # Log.info(processbar3(10*60))
 
 
 def get_for_html():
